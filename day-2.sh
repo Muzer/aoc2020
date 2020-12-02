986867ea6966213eb71c05d99f318637ff5d6bb3
Murray Colpman <muzer@tim32.org>
2020-12-02 14:13:52 +0000

    Finish day 2 part 1

--- day-2.sh
+++ day-2.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+total=0
+
 while read -r line; do
-  
+  range=${line%% *}
+  start=${range%-*}
+  end=${range#*-}
+  char=${line#* }
+  char=${char%: *}
+  password=${line#*: }
+
+  justchar=${password//[^$char]}
+
+  if [ "${#justchar}" -ge "$start" ] && [ "${#justchar}" -le "$end" ]; then
+    let total++
+  fi
 done
+
+echo "$total"
